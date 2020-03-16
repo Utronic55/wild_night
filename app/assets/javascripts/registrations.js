803d04d20cb9b62dc75b8e4bc2a3e2f1e8ee7720
Utronic55 <halfboiled5@gmail.com>
2020-03-16 19:05:29 +0900

    incremental

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -3,7 +3,6 @@ $(function() {
     let html = `
       <div class="user clearfix">
         <p class="user__name">${user.name}</p>
-        <div class="user-search-add user__btn user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>
     `;
     $("#user-search-result").append(html);
@@ -25,35 +24,20 @@ $(function() {
       data: { keyword: input },
       dataType: "json"
     })
-      .done(function(users) {
-        $("#user-search-result").empty();
-        if (users.length !== 0) {
-          users.forEach(function(user) {
-            addUser(user);
-          });
-        } else if (input.length == 0) {
-          return false;
-        } else {
-          addNoUser();
-        }
-      })
-      .fail(function() {
-        alert("通信エラーです。ユーザーが表示できません。");
-      });
-  });
-  $(document).on("click", ".user__btn--add", function() {
-    console.log
-    const userName = $(this).attr("data-user-name");
-    const userId = $(this).attr("data-user-id");
-    $(this)
-      .parent()
-      .remove();
-    addDeleteUser(userName, userId);
-    addMember(userId);
-  });
-  $(document).on("click", ".user__btn--remove", function() {
-    $(this)
-      .parent()
-      .remove();
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
+    })
+    .fail(function() {
+      alert("通信エラーです。ユーザーが表示できません。");
+    });
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
-    
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] )
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def search
