649188a12ab8dfa5a56ccebc0605861a2d28a8f7
Utronic55 <halfboiled5@gmail.com>
2020-03-19 19:51:22 +0900

    js3

--- app/assets/javascripts/genre.js
+++ app/assets/javascripts/genre.js
@@ -1,46 +1,36 @@
-$(function(){
-  // カテゴリーセレクトボックスのオプションを作成
-  function appendOption(genre){
-    var html = `<option value="${genre.id}" data-genre="${genre.id}">${genre.name}</option>`;
-    return html;
-  }
-  // 子カテゴリーの表示作成
-  function appendChidrenBox(insertHTML){
-    var SelectHtml = '';
-    SelectHtml = `<div class='select-default__added' id= 'wrapper' >
-                    <div class='select-default__box'>
-                      <select class="select-default__box--select" id="genre" name="user[genre_id]" required:true>
-                        <option value="---" data-genre="---">---</option>
-                        ${insertHTML}
-                      <select>
-                      <i class='fas fa-chevron-down select-default__box--arrow-down'></i>
-                    </div>
-                  </div>`;
-    $('#genre').append(SelectHtml);
-  }
-  // 親カテゴリー選択後のイベント
-  $('#genre-search-result').on('change', function(){
-    var genre = document.getElementById('genre').value; //選択された親カテゴリーの名前を取得
-    if (genre != "---"){ //親カテゴリーが初期値でないことを確認
-      $.ajax({
-        url: '/items/get_genre',
-        type: 'GET',
-        data: { name: genre },
-        dataType: 'json'
-      })
-      .done(function(){
-        $('#wrapper').remove(); //親が変更された時、子以下を削除するする
-        var insertHTML = '';
-        .forEach(function(){
-          insertHTML += appendOption();
-        });
-        appendChidrenBox(insertHTML);
-      })
-      .fail(function(){
-        alert('カテゴリー取得に失敗しました');
-      })
-    }else{
-      $('#wrapper').remove(); //親カテゴリーが初期値になった時、子以下を削除するする
-    }
+var searchItem = '.search_item';   // 絞り込む項目を選択するエリア
+var listItem = '.list_item';       // 絞り込み対象のアイテム
+var hideClass = 'is-hide';         // 絞り込み対象外の場合に付与されるclass名
+var activeClass = 'is-active';     // 選択中のグループに付与されるclass名
+
+$(function() {
+    // 絞り込みを変更した時
+    $(searchItem).on('click', function() {
+        $(searchItem).removeClass(activeClass);
+        var group = $(this).addClass(activeClass).data('group');
+        search_filter(group);
+    });
   });
-});
+
+// /**
+//  * リストの絞り込みを行う
+//  * @param {String} group data属性の値
+//  */
+function search_filter(group) {
+  console.log('ok')
+    // 非表示状態を解除
+    $(listItem).removeClass(hideClass);
+    // 値が空の場合はすべて表示
+    if(group === '') {
+        return;
+    }
+    // リスト内の各アイテムをチェック
+    for (var i = 0; i < $(listItem).length; i++) {
+        // アイテムに設定している項目を取得
+        var itemData = $(listItem).eq(i).data('group');
+        // 絞り込み対象かどうかを調べる
+        if(itemData !== group) {
+            $(listItem).eq(i).addClass(hideClass);
+        }
+    }
+}
