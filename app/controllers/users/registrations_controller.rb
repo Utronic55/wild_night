b5dc66f10d6be2c1d3cff08b255556c53d147dcc
Utronic55 <halfboiled5@gmail.com>
2020-03-24 13:33:49 +0900

    youtube

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,6 +15,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(user_params)
+    url = params[:user][:youtube_url]
+    url = url.last(11)
+    @user.youtube_url = url
     unless @user.valid?
       flash.now[:alert] = @user.errors.full_messages
       render :new and return
